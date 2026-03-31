176d68c89fc7dc57305b1fb4f5501031c81ceab8
Dr. Otto Dobretsberger <OttoD@eps-software.com>
2026-03-30 22:18:37 -0500

    Fix GitHub Pages PWA standalone install

--- vite.config.ts
+++ vite.config.ts
@@ -20,25 +20,25 @@ export default defineConfig({
         start_url: '/PWRLFT/',
         icons: [
           {
-            src: '/icon-192.png',
+            src: 'icon-192.png',
             sizes: '192x192',
             type: 'image/png',
             purpose: 'any'
           },
           {
-            src: '/icon-512.png',
+            src: 'icon-512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any'
           },
           {
-            src: '/icon-maskable-192.png',
+            src: 'icon-maskable-192.png',
             sizes: '192x192',
             type: 'image/png',
             purpose: 'maskable'
           },
           {
-            src: '/icon-maskable-512.png',
+            src: 'icon-maskable-512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'maskable'
